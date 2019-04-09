a097880df9c1757d9f891b620635b173f43cdce7
Sergii Gavryliuk <draiker.ds@gmail.com>
2019-04-09 14:24:36 +0300

    Add comments

--- frontend.tf
+++ frontend.tf
@@ -1,43 +1,22 @@
-/*resource "google_storage_bucket" "frontend" {
-  count         = "${var.frontend_count}"
-  name          = "${var.frontend_name}-${count.index}"
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page = "404.html"
-  }
-
-}
-
-resource "google_compute_target_pool" "default" {
-  project          = "${var.project}"
-  name             = "lbfrontend"
-  instances = ["${google_compute_instance.${var.frontend_name}.*.self_link}"]
-
-  region           = "${var.region}"
-  session_affinity = "NONE"
-
-  health_checks = [
-    "${google_compute_http_health_check.default.name}",
-  ]
-}
-*/
-
 resource "google_compute_global_forwarding_rule" "default" {
   name       = "default-rule"
   target     = "${google_compute_target_http_proxy.default.self_link}"
   port_range = "80"
 }
 
+# Create Load balancer for frontend buckets
 resource "google_compute_target_http_proxy" "default" {
   name        = "http-proxy"
-  description = "a description"
+  description = "Load balancer for frontend buckets"
   url_map     = "${google_compute_url_map.urlmap.self_link}"
 }
 resource "google_compute_url_map" "urlmap" {
   name        = "lbfrontend"
-  description = "a description"
+  description = "URL map for frontend"
+
+  # Set the first bucket as default
   default_service = "${google_compute_backend_bucket.static.self_link}"
+
   host_rule {
     hosts        = ["bucket1"]
     path_matcher = "allpaths"
@@ -46,6 +25,8 @@ resource "google_compute_url_map" "urlmap" {
     hosts        = ["bucket"]
     path_matcher = "allpaths1"
   }
+
+  # Path matcher for the first bucket
   path_matcher {
     name            = "allpaths"
     default_service = "${google_compute_backend_bucket.static.self_link}"
@@ -55,6 +36,7 @@ resource "google_compute_url_map" "urlmap" {
       service = "${google_compute_backend_bucket.static.self_link}"
     }
   }
+  # Path matcher for the second bucket
   path_matcher {
     name            = "allpaths1"
     default_service = "${google_compute_backend_bucket.static1.self_link}"
